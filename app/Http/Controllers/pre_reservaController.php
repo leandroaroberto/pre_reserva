cbb0299ee85f0932b2c6f541c4a27dd1fad96ed4
ead <ead@ead-02-ubuntu>
2017-07-13 17:31:49 -0300

    Formulario de Pre reserva Formulario com validação ok Gravando nas tabelas pre_reserva e pre_reserva_datas

--- app/Http/Controllers/pre_reservaController.php
+++ app/Http/Controllers/pre_reservaController.php
@@ -4,6 +4,7 @@ namespace ead\Http\Controllers;
 
 use Illuminate\Http\Request;
 use ead\Pre_reserva;
+use ead\Pre_reserva_datas;
 
 class pre_reservaController extends Controller
 {
@@ -19,18 +20,45 @@ class pre_reservaController extends Controller
     
     public function gravar(Request $request){
         $this->validate($request, [
-        'nome' => 'required|max:10',
+        'nome' => 'required|max:50',
         'email' => 'required|max:100|e-mail',
         'professor'=> 'required|max:50',
         'evento' => 'required',
-        'fone' => 'max:10',
+        'fone' => 'max:20',
         'data_reserva' => 'required|date',
         'horario' => 'required'
     ]);
         
-        $dados = $request->all();
+        //$dados = $request->all();
         
-        print_r($dados);
+        $pre_reserva = new Pre_reserva();
+        //$pre_reserva->fill($request->all());
+        $pre_reserva->nome = $request->input('nome');
+        $pre_reserva->email = $request->input('email');
+        $pre_reserva->fone = $request->input('fone');
+        $pre_reserva->professor = $request->input('professor');
+        $pre_reserva->evento = $request->input('evento');
+        $pre_reserva->obs = $request->input('obs');
+        
+        $pre_reserva->save();
+        
+        $pre_reserva_id = $pre_reserva->getKey();
+        
+        //grava na tabela pre_reserva_datas
+        $pre_reserva_datas = new Pre_reserva_datas();
+        //gera a data no formato yyyy-mm-dd hh:mm:ss
+        $data_reserva = $request->input('data_reserva');
+        $data_reserva .= " ". $request->input('horario');
+        
+        $pre_reserva_datas->data_reserva = $data_reserva;
+        $pre_reserva_datas->status = 0;
+        $pre_reserva_datas->pre_reserva_id = $pre_reserva_id;
+        
+        $pre_reserva_datas->save();
+        
+        
+        return redirect()->action('pre_reservaController@index');
+        
     }
     
     
